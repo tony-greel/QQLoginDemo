429495b1b147d8b417eb1a92eeb4ca0b209dfe85
Administrator <1123600675@qq.com>
2017-06-30 18:54:46 +0800

    基础提交

---
+++ app/src/main/java/com/ljj/qqlogindemo/NextActivity.java
@@ -0,0 +1,17 @@
+package com.ljj.qqlogindemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.tencent.tauth.Tencent;
+
+/**
+ * Created by Administrator on 2017/6/30.
+ */
+
+public class NextActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
